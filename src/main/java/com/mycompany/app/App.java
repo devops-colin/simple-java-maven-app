1a84d811567cdcd3e360a2a6fd4daff722a978c1
colin <devops.colin.anthony@gmail.com>
2021-03-14 14:21:02 +0000

    updated code and test code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Bulu!";
+    private final String message = "Hello Lulu!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Bulu!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Lulu!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello Bulu!\"");
+            fail("\"message\" is not \"Hello Lulu!\"");
         }
     }
 
